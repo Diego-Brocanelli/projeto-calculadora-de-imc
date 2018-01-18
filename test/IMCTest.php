674498b29f99123a164d3343302aad89fa0e7d52
Diego Brocanelli <diegod2@msn.com>
2018-01-18 21:28:39 -0200

    inclusão de doc block

--- test/IMCTest.php
+++ test/IMCTest.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 use DiegoBrocanelli\Calcular\IMC;
 use PHPUnit\Framework\TestCase;
 
+/**
+ * Diego Brocanelli <diegod2@msn.com>
+ */
 final class IMCTest extends TestCase
 {
     const ALTURA = 1.73;
